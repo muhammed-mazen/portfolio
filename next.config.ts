e6bdb6847f697bac32f4eaeff4d414313fed15f8
Muhammed Mazen Hafez <101628652+muhammed-mazen@users.noreply.github.com>
2025-07-24 17:26:39 +0300

    deploy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import { NextConfig } from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = {};
-
 const withNextIntl = createNextIntlPlugin();
+
+const nextConfig: NextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true, // important for static export
+    },
+    // basePath: '/your-repo-name', // Uncomment ONLY if not using a custom domain
+};
+
 export default withNextIntl(nextConfig);
